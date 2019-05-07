87e19a417642d87c20f19d57f45b7b7371030589
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-05-07 14:02:09 -0400

    alterações
    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+<header class="masthead" style="background-image:url({{ asset('img/year.jpg') }})">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -16,7 +16,11 @@
                     @endif
 
                     Você está Logado! <i class="far fa-smile-beam fa-spin"></i> 
-
+                    <h3> Bem Vindo!! {{ Auth::user()->name }}</h3>
+                    <ul>
+                        <li>Clique no botão verde caso desejar explorar nosso blog voltado para comunidade do nosso amado Boku No Hero Academy</li>
+                        <li>Caso preferir saia apertando o botão de "Sair"</li>
+                    </ul>
                 </div>
                 <div class="card-footer text-muted">
                     <a href="home/poslog"><button  type="button" class="btn btn-success btn-lg">Explorar</button></a>
@@ -26,5 +30,5 @@
         </div>
     </div>
 </div>
-
+</header>
 @endsection
