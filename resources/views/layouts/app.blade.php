dab17328e87ae1dbd40be808c2185bcd1022ed4b
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-05-08 16:57:44 -0400

    atualizando app
    renomeando os scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,17 +9,25 @@
     <title>{{ config('app.name', 'BNHA') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/clean-blog.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/clean-blog.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jqBootstrapValidation.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/bootstrap.bundle.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/bootstrap.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <!-- Bootstrap core CSS 
+    <!-- Bootstrap core CSS
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
      <!-- Custom fonts for this template -->
-    
+
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
@@ -33,6 +41,8 @@
     <main class="py-4">
         @yield('content')
     </main>
-</body>
 
-</html>
+
+     </body>
+
+ </html>
