b42dafbf1a83bec1026ad765c69ba3e70c654885
Adler Moraes <adlercmoraes98@gmail.com>
2019-05-10 19:36:50 -0400

    Modelagem das paginas about e contato

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -34,30 +34,21 @@
 
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url({{ asset('img/old.jpg') }})">
+  <header class="masthead" style="background-image: url({{ asset('img/aboutimg.png') }})">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-            <h1>BNHA</h1>
-            <span class="subheading">Comunidade e Loja</span>
+            <h1>Sobre o Site</h1>
+            <span class="subheading">Este blog foi montado por 2 amigos que amam Boku no Hero/My Hero Academya para
+            trazer novidades a respeito do mangá e anime e disponibilizar produtos personalizados da franquia.</span>
           </div>
         </div>
       </div>
     </div>
   </header>
 
-  <!-- Main Content -->
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-md-10 mx-auto">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
-      </div>
-    </div>
-  </div>
 
   <hr>
 
