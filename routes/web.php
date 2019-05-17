f86e2134b093c0dc60c12bc01864fdc4869110d3
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-05-17 01:48:36 -0400

    alterando migations
    adicionando parametros ao banco de dados

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('about2', function () {
+    return view('about2');
+});
 
 Route::get('/poslog', 'PosLogController@index')->name('poslog');
 
